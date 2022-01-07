ac41d1bf92d9098af366366271b3c18c9605d4d2
wdeckler <wdeckler@gmail.com>
2022-01-07 12:02:20 -0700

    added grads and sides

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const PI = Math.PI, PI2 = PI * 2;
 const H = 500, W = 500;
+const PREC = 4;
 
 const x_center = W / 2, y_center = H / 2;
 
 var can = null;
 var dc = null;
 
-var degs = 0.0, rads = 0.0;
+var degs = 0.0, rads = 0.0, grad;
 
 var sin = 0.0, cos = 0.0, tan = 0.0;
 var sec = 0.0, csc = 0.0, cot = 0.0;
@@ -14,6 +15,7 @@ var sec = 0.0, csc = 0.0, cot = 0.0;
 var A = 0.0, B = 0.0, C = 1.0;
 
 var degs_edt = null, rads_edt = null;
+var grad_edt = null;
 
 var sin_edt = null, cos_edt = null, tan_edt = null;
 var sec_edt = null, csc_edt = null, cot_edt = null;
@@ -33,6 +35,12 @@ function init()
 	
 	rads_edt = document.getElementById("rads_edt");
 	rads_edt.onchange = function(e)
+	{
+		setAngle(e.target.id);
+	}
+	
+	grad_edt = document.getElementById("grad_edt");
+	grad_edt.onchange = function(e)
 	{
 		setAngle(e.target.id);
 	}
@@ -72,6 +80,32 @@ function init()
 	{
 		setInverse(e.target.id);
 	}
+	
+	A_edt = document.getElementById("A_edt");
+	A_edt.onchange = function(e)
+	{
+		setSide(e.target.id);
+	}
+	
+	B_edt = document.getElementById("B_edt");
+	B_edt.onchange = function(e)
+	{
+		setSide(e.target.id);
+	}
+	
+	C_edt = document.getElementById("C_edt");
+	C_edt.onchange = function(e)
+	{
+		setSide(e.target.id);
+	}
+	
+	degs = 36.87;
+	degs_edt.value = degs.toPrecision(PREC);
+	C = 5;
+	C_edt.value = C.toPrecision(PREC);
+	
+	setAngle("degs_edt");
+	setSide();
 }
 
 function graphIt()
@@ -119,38 +153,54 @@ function setAngle(sentby)
 	case "degs_edt":
 		degs = degs_edt.value;
 		
+		grad = degs * 10/9;
+		grad_edt.value = grad.toPrecision(PREC);
+		
 		rads = degs * PI/180;
-		rads_edt.value = rads.toPrecision(4);
+		rads_edt.value = rads.toPrecision(PREC);
 	
 		break;
 	case "rads_edt":
 		rads = rads_edt.value;
 		
 		degs = rads * 180/PI;
-		degs_edt.value = degs.toPrecision(4);
+		degs_edt.value = degs.toPrecision(PREC);
 	
+		grad = degs * 10/9;
+		grad_edt.value = grad.toPrecision(PREC);
+		break;
+	case "grad_edt":
+		grad = grad_edt.value;
+		
+		degs = grad * 9/10;
+		degs_edt.value = degs.toPrecision(PREC);
+	
+		rads = degs * PI/180;
+		rads_edt.value = rads.toPrecision(PREC);
 		break;
 	default:
 	}
 	sin = Math.sin(rads);
-	sin_edt.value = sin.toPrecision(4);
+	sin_edt.value = sin.toPrecision(PREC);
 	
 	cos = Math.cos(rads);
-	cos_edt.value = cos.toPrecision(4);
+	cos_edt.value = cos.toPrecision(PREC);
 	
 	tan = Math.tan(rads);
-	tan_edt.value = tan.toPrecision(4);
+	tan_edt.value = tan.toPrecision(PREC);
 	
 	csc = 1 / sin;
-	csc_edt.value = csc.toPrecision(4);
+	csc_edt.value = csc.toPrecision(PREC);
 	
 	sec = 1 / cos;
-	sec_edt.value = sec.toPrecision(4);
+	sec_edt.value = sec.toPrecision(PREC);
 	
 	cot = 1 / tan;
-	cot_edt.value = cot.toPrecision(4);
+	cot_edt.value = cot.toPrecision(PREC);
 	
 	graphIt();
+	
+	setSide();
 }
 
 function setInfo(sentby)
@@ -161,63 +211,74 @@ function setInfo(sentby)
 			sin = sin_edt.value;
 			
 			rads = Math.asin(sin);
-			rads_edt.value = rads.toPrecision(4);
+			rads_edt.value = rads.toPrecision(PREC);
 			
 			degs = rads * 180 / PI;
-			degs_edt.value = degs.toPrecision(4);
+			degs_edt.value = degs.toPrecision(PREC);
 
+			grad = degs * 10/9;
+			grad_edt.value = grad.toPrecision(PREC);
+		
 			cos = Math.cos(rads);
-			cos_edt.value = cos.toPrecision(4);
+			cos_edt.value = cos.toPrecision(PREC);
 
 			tan = Math.tan(rads);
-			tan_edt.value = tan.toPrecision(4);
+			tan_edt.value = tan.toPrecision(PREC);
 			
 			break;
 		case "cos_edt":
 			cos = cos_edt.value;
 			
 			rads = Math.acos(cos);
-			rads_edt.value = rads.toPrecision(4);
+			rads_edt.value = rads.toPrecision(PREC);
 			
 			degs = rads * 180 / PI;
-			degs_edt.value = degs.toPrecision(4);
+			degs_edt.value = degs.toPrecision(PREC);
 
+			grad = degs * 10/9;
+			grad_edt.value = grad.toPrecision(PREC);
+		
 			sin = Math.sin(rads);
-			sin_edt.value = sin.toPrecision(4);
+			sin_edt.value = sin.toPrecision(PREC);
 		
 			tan = Math.tan(rads);
-			tan_edt.value = tan.toPrecision(4);
+			tan_edt.value = tan.toPrecision(PREC);
 			
 			break;
 		case "tan_edt":
 			tan = tan_edt.value;
 			
 			rads = Math.atan(tan);
-			rads_edt.value = rads.toPrecision(4);
+			rads_edt.value = rads.toPrecision(PREC);
 
 			degs = rads * 180 / PI;
-			degs_edt.value = degs.toPrecision(4);
+			degs_edt.value = degs.toPrecision(PREC);
 
+			grad = degs * 10/9;
+			grad_edt.value = grad.toPrecision(PREC);
+		
 			cos = Math.cos(rads);
-			cos_edt.value = cos.toPrecision(4);
+			cos_edt.value = cos.toPrecision(PREC);
 			
 			sin = Math.sin(rads);
-			sin_edt.value = sin.toPrecision(4);
+			sin_edt.value = sin.toPrecision(PREC);
 			
 			break;
 		default:
 	}
 			
 	csc = 1 / sin;
-	csc_edt.value = csc.toPrecision(4);
+	csc_edt.value = csc.toPrecision(PREC);
 	
 	sec = 1 / cos;
-	sec_edt.value = sec.toPrecision(4);
+	sec_edt.value = sec.toPrecision(PREC);
 
 	cot = 1 / tan;
-	cot_edt.value = cot.toPrecision(4);
+	cot_edt.value = cot.toPrecision(PREC);
 	
 	graphIt();
+	
+	setSide();
 }
 
 function setInverse(sentby)
@@ -228,61 +289,83 @@ function setInverse(sentby)
 			csc = csc_edt.value;
 
 			rads = Math.asin(1/csc);
-			rads_edt.value = rads.toPrecision(4);
+			rads_edt.value = rads.toPrecision(PREC);
 
 			degs = rads * 180 / PI;
-			degs_edt.value = degs.toPrecision(4);
+			degs_edt.value = degs.toPrecision(PREC);
 
+			grad = degs * 10/9;
+			grad_edt.value = grad.toPrecision(PREC);
+		
 			sec = 1 / Math.cos(rads);
-			sec_edt.value = sec.toPrecision(4);
+			sec_edt.value = sec.toPrecision(PREC);
 
 			cot = 1 / Math.tan(rads);
-			cot_edt.value = cot.toPrecision(4);
+			cot_edt.value = cot.toPrecision(PREC);
 
 			break;
 		case "sec_edt":
 			sec = sec_edt.value;
 
 			rads = Math.acos(1/sec);
-			rads_edt.value = rads.toPrecision(4);
+			rads_edt.value = rads.toPrecision(PREC);
 
 			degs = rads * 180 / PI;
-			degs_edt.value = degs.toPrecision(4);
+			degs_edt.value = degs.toPrecision(PREC);
 
+			grad = degs * 10/9;
+			grad_edt.value = grad.toPrecision(PREC);
+		
 			csc = 1 / Math.sin(rads);
-			csc_edt.value = csc.toPrecision(4);
+			csc_edt.value = csc.toPrecision(PREC);
 
 			cot = 1 / Math.tan(rads);
-			cot_edt.value = cot.toPrecision(4);
+			cot_edt.value = cot.toPrecision(PREC);
 
 			break;
 		case "cot_edt":
 			cot = cot_edt.value;
 
 			rads = Math.atan(1/tan);
-			rads_edt.value = rads.toPrecision(4);
+			rads_edt.value = rads.toPrecision(PREC);
 
 			degs = rads * 180 / PI;
-			degs_edt.value = degs.toPrecision(4);
+			degs_edt.value = degs.toPrecision(PREC);
 
+			grad = degs * 10/9;
+			grad_edt.value = grad.toPrecision(PREC);
+		
 			sec = 1 / Math.cos(rads);
-			cos_edt.value = cos.toPrecision(4);
+			cos_edt.value = cos.toPrecision(PREC);
 
 			csc = 1 / Math.sin(rads);
-			sin_edt.value = sin.toPrecision(4);
+			sin_edt.value = sin.toPrecision(PREC);
 
 			break;
 		default:
 	}
 
 	sin = 1 / csc;
-	sin_edt.value = sin.toPrecision(4);
+	sin_edt.value = sin.toPrecision(PREC);
 
 	cos = 1 / sec;
-	cos_edt.value = cos.toPrecision(4);
+	cos_edt.value = cos.toPrecision(PREC);
 
 	tan = 1 / cot;
-	tan_edt.value = tan.toPrecision(4);
+	tan_edt.value = tan.toPrecision(PREC);
 
 	graphIt();
+	
+	setSide();
+}
+
+function setSide()
+{
+	C = C_edt.value;
+
+	A = sin * C;
+	A_edt.value = A.toPrecision(PREC);
+	
+	B = cos * C;
+	B_edt.value = B.toPrecision(PREC);
 }
